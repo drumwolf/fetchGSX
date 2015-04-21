33a5667554c53b72182efccd2e317d284f79b5f3
John Lee <drumwolfmail@yahoo.com>
2015-04-20 19:02:14 -0700

    rendered it in module format

--- parse-gsx-ajax.js
+++ parse-gsx-ajax.js
@@ -1,18 +1,22 @@
-var ParseGSXAjax = function(spreadsheetID, callback) {
-  var url = "http://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
-  var ajax = $.ajax(url);
-  var processData = function(res){
-    var finalData = [];
-    res.feed.entry.forEach(function(entry){
-      var parsedObject = {};
-      for (var key in entry) {
-        if (key.substring(0,4) === "gsx$") {
-          parsedObject[key.slice(4)] = entry[key]["$t"];
+var ParseGSXAjax = (function() {
+  return {
+    init: function(spreadsheetID, callback) {
+      var url = "http://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+      var ajax = $.ajax(url);
+      $.when(ajax).then(this.processData);
+    },
+    processData: function(res) {
+      var finalData = [];
+      res.feed.entry.forEach(function(entry){
+        var parsedObject = {};
+        for (var key in entry) {
+          if (key.substring(0,4) === "gsx$") {
+            parsedObject[key.slice(4)] = entry[key]["$t"];
+          }
         }
-      }
-      finalData.push(parsedObject);
-    });
-    callback(finalData);
-  }
-  $.when(ajax).then(processData);
-}
+        finalData.push(parsedObject);
+      });
+      callback(finalData);
+    }
+  };
+})();
